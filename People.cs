9193957087a4941d2b7ae6f088f106f3562dd569
khas <khas@MYLABPC-E10>
2022-12-28 15:15:42 +0300

    Add project files.

---
+++ People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Egzeriz
+{
+    internal class People
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+
+        public People() 
+        {
+        
+
+        
+        }
+
+        public string Name { get; set; }    
+        public string SurName { get; set; } 
+        public int Lenght { get; set;}
+        public int Kilo { get; set; }
+        public int Years { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OOP.Egzeriz;
+
+internal class Program
+{
+    // insan isimli bir sınıf içinde isim, soyisim, boy, kilo, yaş alanlarını tanımlayıp, bir kurucu metot içinde bu alanlar için varsayılan değer atamalarını yapacağız. son olarak da " Tanis" isimli metotla girilen bilgileri kullanıcıya belli, güzel, açıklayıcı bir şekilde görüntüleyen bir program yazınız..
+
+    // örnek: Ben Şermin ATMACA.. boyum: 1,69.. kilom: 67 kg.. yaşım:29'dur
+
+    private static void Main(string[] args)
+
+    {
+        People Name = new People();
+        People
+        Console.WriteLine(" Benim adım : " + People.)
+
+    }
+     
+    
+
+}
